ea3aaf424ee975217396ccbe058166b31e59eb56
Arnaudboy <r.boyer895@laposte.net>
2019-07-15 21:12:07 +0200

    exercice 4 fini

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -6,6 +6,23 @@ def shout (string)
 	return string.upcase
 end
 
-def repeat (string, *n)
-	return String(string, b=2, n)
+def repeat (truc, n = 2, *p)
+    if p != nil 
+        return (truc + " ")*(n - 1) + truc 
+    else 
+        return (truc + " ")*(p - 1) + truc
+    end 
+end   
+
+def start_of_word(string, n)
+	return string[0..(n-1)]
+end
+
+def first_word (string)
+	return string.split(" ")[0]
+end 
+
+def titleize (string)
+	nocaps = "and", "or", "the"
+	return string.capitalize.split(" ").map { |word| nocaps.include?(word) ? word : word.capitalize }.join(" ")
 end 
